d46b00b52bd537b7d65f731ed51d9be15e0267cc
李小义 <unsplash@qq.com>
2018-02-09 16:58:04 +0800

    作业

--- app/src/main/java/com/liqy/ndkbuildapp/MainActivity.java
+++ app/src/main/java/com/liqy/ndkbuildapp/MainActivity.java
@@ -1,9 +1,14 @@
 package com.liqy.ndkbuildapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
+/**
+ *
+ * 由静态注册变成动态注册
+ *
+ */
 public class MainActivity extends AppCompatActivity {
 
     //使用静态代码块引入cpp
@@ -11,14 +16,13 @@ public class MainActivity extends AppCompatActivity {
         System.loadLibrary("hello-jni");
     }
 
-
     public TextView hello;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        hello=(TextView)findViewById(R.id.hello);
+        hello = (TextView) findViewById(R.id.hello);
         hello.setText(jniTellMeWhy("1510C"));
     }
 
